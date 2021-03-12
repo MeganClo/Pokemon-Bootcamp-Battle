361b15712d94320d5868a7d96970259a950cfdc0
Megs <Megs.close@gmail.com>
2021-03-12 13:43:12 -0800

    enemy health is responding to user attack

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,10 @@ var attack2 = document.getElementById("2");
 var attack3 = document.getElementById("3");
 var attack4 = document.getElementById("4");
 
+// var attackVal = document.querySelectorAll("btn");
+// console.log(attackVal);
+
+
 // targetting the text to update heath/attack value
 var emenyHealthText = document.getElementById("enemyHealth");
 var userHealthText = document.getElementById("userHealth");
@@ -19,39 +23,48 @@ var enemyAttackText = document.getElementById("enemyAttack");
 var userAttackText = document.getElementById("userAttack");
 
 // updated health text
-emenyHealthText.textContent = enemyHealth;
-userHealthText.textContent = userHealth;
+var updateHealth = function() {
+    emenyHealthText.textContent = enemyHealth;
+    userHealthText.textContent = userHealth;
+};
 
-
-var userFight = function() {
+var userFight = function(attackVal) {
     console.log("fighting!");
-}
+    enemyHealth = enemyHealth-attackVal;
+    updateHealth ();
+};
 
 
 attack1.addEventListener("click", function() {
     console.log("getting there");
     var attackVal = document.getElementById("1").value;
     console.log(attackVal);
-    userFight();
+    userFight(attackVal);
+    console.log(enemyHealth);
 });
 
 attack2.addEventListener("click", function() {
     console.log("getting there");
     var attackVal = document.getElementById("2").value;
     console.log(attackVal);
-    userFight();
+    userFight(attackVal);
+    console.log(enemyHealth);
 });
 
 attack3.addEventListener("click", function() {
     console.log("getting there");
     var attackVal = document.getElementById("3").value;
     console.log(attackVal);
-    userFight();
+    userFight(attackVal);
+    console.log(enemyHealth);
 });
 
 attack4.addEventListener("click", function() {
     console.log("getting there");
     var attackVal = document.getElementById("4").value;
     console.log(attackVal);
-    userFight();
+    userFight(attackVal);
+    console.log(enemyHealth);
 });
+
+updateHealth();
