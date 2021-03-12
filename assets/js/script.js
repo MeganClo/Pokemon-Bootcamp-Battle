3747981829003b9b8eae7fd9ca787d2429c0522c
Megs <Megs.close@gmail.com>
2021-03-12 12:32:17 -0800

    values are getting taken from buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,45 +12,46 @@ var attack2 = document.getElementById("2");
 var attack3 = document.getElementById("3");
 var attack4 = document.getElementById("4");
 
-// getting the value from each button
-var attack1val = document.getElementById("1").value;
-var attack2val = document.getElementById("2").value;
-var attack3val = document.getElementById("3").value;
-var attack4val = document.getElementById("4").value;
-
 // targetting the text to update heath/attack value
 var emenyHealthText = document.getElementById("enemyHealth");
 var userHealthText = document.getElementById("userHealth");
 var enemyAttackText = document.getElementById("enemyAttack");
 var userAttackText = document.getElementById("userAttack");
 
+// updated health text
 emenyHealthText.textContent = enemyHealth;
 userHealthText.textContent = userHealth;
 
 
-
-
-var userfight = function() {
+var userFight = function() {
     console.log("fighting!");
 }
 
 
 attack1.addEventListener("click", function() {
     console.log("getting there");
-    fight();
+    var attackVal = document.getElementById("1").value;
+    console.log(attackVal);
+    userFight();
 });
 
 attack2.addEventListener("click", function() {
     console.log("getting there");
-    fight();
+    var attackVal = document.getElementById("2").value;
+    console.log(attackVal);
+    userFight();
 });
 
 attack3.addEventListener("click", function() {
     console.log("getting there");
-    fight();
+    var attackVal = document.getElementById("3").value;
+    console.log(attackVal);
+    userFight();
 });
 
 attack4.addEventListener("click", function() {
     console.log("getting there");
-    fight();
+    var attackVal = document.getElementById("4").value;
+    console.log(attackVal);
+    userFight();
 });
