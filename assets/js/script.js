c1cc485f3e40e0ced5b27e7e774e51fa50c1c4cc
Megs <Megs.close@gmail.com>
2021-03-12 11:41:30 -0800

    thinking out fight functions

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+// setting the health both parties
+var userHeath = 100;
+var enemyHealth = 100;
+
+// setting attack value
+var userAttack = "";
+var enemeyAttack = "";
+
+// targetting the buttons to attack
+var attack1 = document.getElementById("1");
+var attack2 = document.getElementById("2");
+var attack3 = document.getElementById("3");
+var attack4 = document.getElementById("4");
+
+// targetting the text to update heath/attack value
+
+
+
+var fight = function() {
+
+}
+
+
+attack1.addEventListener("click", function() {
+    console.log("getting there");
+});
+
+attack2.addEventListener("click", function() {
+    console.log("getting there");
+});
+
+attack3.addEventListener("click", function() {
+    console.log("getting there");
+});
+
+attack4.addEventListener("click", function() {
+    console.log("getting there");
+});
