e1945e0a8f7f7f5a13b304d9c7bb29f90cb66082
Megs <Megs.close@gmail.com>
2021-03-16 20:45:16 -0700

    trying to fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,6 @@
 // getting username from landing page via local storage
 var userName = localStorage.getItem("Name");
 console.log(userName);
-// Click function for show the Modal
-
-$(".show").on("click", function(){
-    $(".mask").addClass("active");
-  });
-  
-  // Function for close the Modal
-  
-  function closeModal(){
-    $(".mask").removeClass("active");
-  }
-  
-  // Call the closeModal function on the clicks/keyboard
-  
-  $(".close, .mask").on("click", function(){
-    closeModal();
-  });
-  
-  $(document).keyup(function(e) {
-    if (e.keyCode == 27) {
-      closeModal();
-    }
-  });
-
 
 // setting the health both parties
 var enemyHealth = 100;
@@ -64,13 +40,16 @@ var attack4 = document.getElementById("move4");
 // targeting all the buttons
 var attackButtons = document.getElementsByClassName("attackButtons");
 
+// targeting random user information placeholders
+var randomUser = document.querySelector("#randomPicture");
+var randomUpdates = document.querySelector("#randomUpdates");
+
 // targetting the text to update attack value
 var enemyAttackText = document.getElementById("enemyAttack");
 var userAttackText = document.getElementById("userAttack");
 
 // var emenyHealthText = document.getElementById("enemyHealth");
 // var userHealthText = document.getElementById("userHealth");
-
 
 // adding moves to the page
 var randomUser = document.querySelector("#opponentHeader")
@@ -79,14 +58,23 @@ var moveTwo = document.querySelector("#move2")
 var moveThree = document.querySelector("#move3")
 var moveFour = document.querySelector("#move4")
 
+// targetting user's and opponents pokemons and pokemons images placeholders
 var userPokemon = document.querySelector("#userPokemonName")
 var userPokemonPlaceholder = document.querySelector("#userPokemonPicture")
+var opponentPokemon = document.querySelector("#randomPokemonName")
+var opponentPokemonPlaceholder = document.querySelector("#randomPokemonPicture")
 
 // creating a random user pokemon
 var pokemon = ["pikachu", "charmander", "bulbasaur", "eevee", "oddish", "squirtle", "bellsprout", "butterfree", "pidgeotto", "jigglypuff"]
 const random = Math.floor(Math.random() * pokemon.length);
 var userRandomPokemon = pokemon[random];
-console.log(userRandomPokemon)
+console.log(userRandomPokemon);
+
+//creating opponent random pokemon
+var opponentRandomPokemon = pokemon[Math.floor(Math.random() * pokemon.length)]; //Get a random field name from the array
+while (pokemon == opponentRandomPokemon) {
+    var opponentPokemon = pokemon[Math.floor(Math.random() * pokemon.length)];
+};
 
 // updated health text
 var updateHealth = function() {
@@ -247,6 +235,7 @@ attack4.addEventListener("click", function() {
 });
 
 
+// getting random user information from API
 let getUserInfo = () => {
     fetch("https://randomuser.me/api/").then((response) => {
         response.json().then((data) => {
@@ -257,6 +246,7 @@ let getUserInfo = () => {
 };
 
 
+// displaying random user information from API
 let displayRepos = (repos) => {
     // console.log(repos);
 
@@ -265,78 +255,97 @@ let displayRepos = (repos) => {
         // loop over user name
         let nameObject = repos[i].name
 
+        
         const name = Object.values(nameObject)
         // create an elememt to hold random user name
         let randomUserName = document.createElement("div");
         randomUserName.textContent = name[1] + " " + name[2];
-
         randomUser.appendChild(randomUserName)
-
         // loop over user country
         let countryObject = repos[i].location
-
         const country = Object.values(countryObject)
         // let randomUserCountry = country[3]
         // console.log("I am from " + randomUserCountry)
         let randomUserCountry = document.createElement("div");
         randomUserCountry.textContent = country[3]
-
         randomUser.appendChild(randomUserCountry)
-
         //loop over pictures
         let pictureObject = repos[i].picture
         // console.log(pictureObject)
 
         const picture = Object.values(pictureObject)
         let randomUserImg = document.createElement('img');
+        randomUserImg.setAttribute("class", "circle")
         randomUserImg.setAttribute('src', picture[0]);
+
         // let randomUserPicture = picture[2]
         // console.log(randomUserPicture)
 
         randomUser.appendChild(randomUserImg)
-
     }
 };
 
+// getting and displaying user's random pokemon and moves
 let getCurrentInfo = () => {
     let apiURL = "https://pokeapi.co/api/v2/pokemon/" + userRandomPokemon;
     fetch(apiURL).then((response) => {
         response.json().then((data) => {
-
             //displaying random user pokemon name
             userPokemon.textContent = userRandomPokemon
-
             //displaying random user pokemon picture
             var userPokemonImage = document.createElement("img")
             var imageURL = "./assets/images/" + userRandomPokemon + ".png"
             userPokemonImage.setAttribute("src", imageURL)
             userPokemonImage.setAttribute("style", "max-width: 15vh")
             console.log(userPokemonImage)
+            // console.log(userPokemonImage)
             userPokemonPlaceholder.appendChild(userPokemonImage)
 
             let movesObject1 = document.createElement("p")
             movesObject1.textContent = data.moves[0].move.name
-
             moveOne.appendChild(movesObject1)
-
             let movesObject2 = document.createElement("p")
             movesObject2.textContent = data.moves[1].move.name
-
             moveTwo.appendChild(movesObject2)
-
             let movesObject3 = document.createElement("p")
             movesObject3.textContent = data.moves[2].move.name
-
             moveThree.appendChild(movesObject3)
-
             let movesObject4 = document.createElement("p")
             movesObject4.textContent = data.moves[3].move.name
-
             moveFour.appendChild(movesObject4)
-
         })
     })
 };
+
+// getting from API and displaying opponent's pokemon and moves
+let opponentInfo = () => {
+    let apiURL = "https://pokeapi.co/api/v2/pokemon/" + opponentRandomPokemon;
+    fetch(apiURL).then((response) => {
+        response.json().then((data) => {
+            console.log(data)
+
+            // displaying opponent random pokemon's name
+            opponentPokemon.textContent = opponentRandomPokemon
+            // console.log(opponentPokemon)
+
+            //displaying oponent's random pokemon picture
+            var opponentPokemonImage = document.createElement("img")
+            var imageURL = "./assets/images/" + opponentRandomPokemon + ".png"
+            opponentPokemonImage.setAttribute("src", imageURL)
+            opponentPokemonImage.setAttribute("style", "max-width: 15vh")
+            // console.log(userPokemonImage)
+            opponentPokemonPlaceholder.appendChild(opponentPokemonImage)
+
+            //displaying radom moves
+            var opponentMove = document.createElement("p")
+            opponentMove.textContent = data.moves[0].move.name
+            console.log(opponentMove)
+
+            randomUpdates.appendChild(opponentMove)
+        })
+    })
+};
+
 
 var gameEnd = function() {
     var score = { 
@@ -351,6 +360,28 @@ var gameEnd = function() {
     localStorage.setItem("highscores", JSON.stringify(highscores));
 };
 
+// Click function for show the Modal
+$(".show").on("click", function(){
+    $(".mask").addClass("active");
+  });
+  
+  // Function for close the Modal
+  
+  function closeModal(){
+    $(".mask").removeClass("active");
+  }
+  
+  // Call the closeModal function on the clicks/keyboard
+  
+  $(".close, .mask").on("click", function(){
+    closeModal();
+  });
+  
+  $(document).keyup(function(e) {
+    if (e.keyCode == 27) {
+      closeModal();
+    }
+  });
 
 
 // var displayScores = function() {
@@ -369,3 +400,4 @@ getCurrentInfo();
 updateHealth();
 getUserInfo();
 $(document).foundation();
+opponentInfo()
