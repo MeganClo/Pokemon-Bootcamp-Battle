f313e15fdbde4cb189a267ac697df6bface9d6a7
Megs <Megs.close@gmail.com>
2021-03-17 14:55:03 -0700

    texting

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,7 +1,13 @@
 var highscoresList = document.getElementById("leaderlist");
 var highscores = JSON.parse(localStorage.getItem("highscores")) || [];
+var noScore = document.getElementById("noScore")
 
 highscoresList.innerHTML =(highscores.map(score => {
     return `<li>${score.name} - ${score.score}</li>`;
 }).join(""));
 
+console.log(highscores.length);
+
+if (highscores.length === 0) {
+    noScore.innerHTML = "No High Scores, yet!"
+};
