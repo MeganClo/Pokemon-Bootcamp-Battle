7a61f1f9113e5bdb31b705432ba1ccb1a499b86b
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-03-17 22:56:21 -0700

    basic modal functionality is ready

--- assets/js/script.js
+++ assets/js/script.js
@@ -358,22 +358,54 @@ var gameEnd = function() {
     localStorage.setItem("highscores", JSON.stringify(highscores));
 };
 
-// Click function for show the Modal
+// // Click function for show the Modal
 // $(".show").on("click", function(){
 //     $(".mask").addClass("active");
 //   });
   
-  // Function for close the Modal
+//   // Function for close the Modal
   
-  function closeModal(){
-    $(".mask").removeClass("active");
+//   function closeModal(){
+//     $(".mask").removeClass("active");
+//   }
+  
+//   // Call the closeModal function on the clicks/keyboard
+  
+//   $(".close, .mask").on("click", function(){
+//     closeModal();
+//   });
+
+// testing new modal
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+// if (enemyHealth <= 0 || userHealth <= 0) {
+    if (1 === 1) {
+    console.log("Hello")
+// btn.onclick = function() {
+  modal.style.display = "block";
+// }
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
-  
-  // Call the closeModal function on the clicks/keyboard
-  
-  $(".close, .mask").on("click", function(){
-    closeModal();
-  });
+}
   
   $(document).keyup(function(e) {
     if (e.keyCode == 27) {
